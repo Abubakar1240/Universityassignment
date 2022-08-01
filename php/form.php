dd0c5a99c9afdf3ee6a4cf6732370940a4ed9254
Abubakar1240 <abubakarrazzaq8811@gmail.com>
2022-08-01 19:51:26 +0500

    first commit

---
+++ php/form.php
@@ -0,0 +1,22 @@
+<?php
+	$FirstName = $_POST['FirstName'];
+	$LastName = $_POST['LastName'];
+	$Email = $_POST['Email'];
+	$Subject = $_POST['Subject'];
+	$Message = $_POST['Message'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(FirstName, LastName, Email, Subject, numberMessage) values(?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssss", $FirstName, $LastName, $Email, $Subject, $Message);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
